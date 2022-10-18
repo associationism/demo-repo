033129a326c119882bdf3a5b4218781aeb4cfc83
Víz Elek <viz@elek.com>
2022-10-18 08:42:43 +0200

    Added .gitignore

--- day02.py
+++ day02.py
@@ -1,1 +1,16 @@
 # basic authentication
+import json
+
+file = open("secrets.json", "r")
+data = file.read()
+file.close()
+
+auth = json.loads(data)
+
+username = input("Adja meg a felhasználónevét: ")
+password = input("Adja meg a jelszavát: ")
+
+if username == auth["username"] and password == auth["password"]:
+    print("Üdv a fedélzeten!")
+else:
+    print("Nincs engedélyezve a belépés!")
